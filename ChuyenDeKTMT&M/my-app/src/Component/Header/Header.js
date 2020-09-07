7fa30219ee9878d9d8d01f786360ebb4d09c2b99
unknown <you@example.com>
2020-09-07 15:50:15 +0700

    push code

---
+++ ChuyenDeKTMT&M/my-app/src/Component/Header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import SideBar from "../SideBar/SideBar";
+const Header = () => {
+  return (
+    <SideBar/>
+    
+  );
+};
+
+export default Header;
---
+++ ChuyenDeKTMT&M/my-app/src/Component/Profile/slider.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
+
+const useStyles = makeStyles({
+  root: {
+    width: 300,
+  },
+});
+
+function valuetext(value) {
+  return `${value}°C`;
+}
+
+export default function SliderProfile() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Typography id="discrete-slider" gutterBottom>
+        HTML
+      </Typography>
+      <Slider
+        defaultValue={90}
+        getAriaValueText={valuetext}
+        aria-labelledby="discrete-slider"
+        valueLabelDisplay="auto"
+        step={10}
+        marks
+        min={10}
+        max={110}
+      />
+      <Typography id="discrete-slider" gutterBottom>
+        CSS
+      </Typography>
+      <Slider
+        defaultValue={90}
+        getAriaValueText={valuetext}
+        aria-labelledby="discrete-slider"
+        valueLabelDisplay="auto"
+        step={10}
+        marks
+        min={10}
+        max={110}
+      />
+      <Typography id="discrete-slider" gutterBottom>
+        JAVA SCRIPT
+      </Typography>
+      <Slider
+        defaultValue={50}
+        getAriaValueText={valuetext}
+        aria-labelledby="discrete-slider"
+        valueLabelDisplay="auto"
+        step={10}
+        marks
+        min={10}
+        max={110}
+      />
+      <Typography id="discrete-slider" gutterBottom>
+        REACTJS
+      </Typography>
+      <Slider
+        defaultValue={60}
+        getAriaValueText={valuetext}
+        aria-labelledby="discrete-slider"
+        valueLabelDisplay="auto"
+        step={10}
+        marks
+        min={10}
+        max={110}
+      />
+    </div>
+  );
+}
